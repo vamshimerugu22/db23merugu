9a5d096f561297e08dabe7e26c19bafe36db10f7
vamshimerugu22 <vamshi.ylgf66@gmail.com>
2021-11-30 21:09:23 -0600

    added login details

--- controllers/pancake.js
+++ controllers/pancake.js
@@ -107,3 +107,39 @@ exports.pancake_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+exports.pancake_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('pancakecreate', { title: 'pancake Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+// Handle building the view for updating a pancake. 
+// query provides the id 
+exports.pancake_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await pancake.findById(req.query.id) 
+        res.render('pancakeupdate', { title: 'pancake Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.pancake_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await pancake.findById(req.query.id) 
+        res.render('pancakedelete', { title: 'pancake Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/pancake.js
+++ routes/pancake.js
@@ -1,9 +1,20 @@
 var express = require('express');
 const pancake_controlers= require('../controllers/pancake'); 
 var router = express.Router();
-
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET home page. */
 router.get('/', pancake_controlers.pancake_view_all_Page );
 
 router.get('/detail', pancake_controlers.pancake_view_one_Page); 
+router.get('/create', pancake_controlers.pancake_create_Page); 
+router.get('/update',secured, pancake_controlers.pancake_update_Page);
+router.get('/delete', pancake_controlers.pancake_delete_Page); 
+
+
 module.exports = router;
